33f45c7f60b040b7189be746135f606f5db044ad
Emre Turgut <emre.turgut@trendyol.com>
2021-12-11 13:06:26 +0300

    coroutine scope

--- src/main/kotlin/com/github/emreturgutce/Hello.kt
+++ src/main/kotlin/com/github/emreturgutce/Hello.kt
@@ -1,15 +1,24 @@
 package com.github.emreturgutce
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 fun main(args: Array<String>) = runBlocking {
 
-    launch {
-        delay(1000L)
-        println("World")
-    }
-    print("Hello ")
-}
+    val scope = CoroutineScope(Dispatchers.Default)
+    val start = System.currentTimeMillis()
 
+    fun showSomeData(scope: CoroutineScope) = scope.launch {
+        delay(500)
+        val end = System.currentTimeMillis()
+        println("Thread: ${Thread.currentThread().name} Time: ${end.minus(start)}ms")
+    }
+
+    val jobs = mutableListOf<Job>()
+
+    repeat(100) {
+        val job = showSomeData(scope)
+        jobs.add(job)
+    }
+
+    jobs.forEach { it.join() }
+}
